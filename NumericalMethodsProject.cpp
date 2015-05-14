4b9d812b28bcba979a0cf33eaef80361342f1ba6
Ignat <iob.veritas@gmail.com>
2015-05-14 21:51:57 +0300

    matrix transform
    transform to almost Jordan form

--- NumericalMethodsProject.cpp
+++ NumericalMethodsProject.cpp
@@ -11,6 +11,11 @@
 
 using namespace std;
 
+bool operator==(const complex<double>& lhs, const complex<double>& rhs){
+	const double epsilon = 1e-7;
+	return (abs(lhs - rhs) < epsilon);
+}
+
 template<class T>
 class Matrix{
 protected:
@@ -29,8 +34,8 @@ public:
 		for (size_t i = 0; i < n; ++i)
 			mtr[i] = new T[m];
 		for (size_t i = 0; i < n; ++i)
-			for (size_t j = 0; j < m; ++j)
-				mtr[i][j] = val;
+		for (size_t j = 0; j < m; ++j)
+			mtr[i][j] = val;
 	}
 
 	Matrix(size_t n, size_t m, T** a){
@@ -39,8 +44,8 @@ public:
 		for (size_t i = 0; i < n; ++i)
 			mtr[i] = new T[m];
 		for (size_t i = 0; i < n; ++i)
-			for (size_t j = 0; j < m; ++j)
-				mtr[i][j] = a[i][j];
+		for (size_t j = 0; j < m; ++j)
+			mtr[i][j] = a[i][j];
 	}
 
 	Matrix(Matrix& a){
@@ -49,8 +54,8 @@ public:
 		for (size_t i = 0; i < r; ++i)
 			mtr[i] = new T[c];
 		for (size_t i = 0; i < r; ++i)
-			for (size_t j = 0; j < c; ++j)
-				mtr[i][j] = a[i][j];
+		for (size_t j = 0; j < c; ++j)
+			mtr[i][j] = a[i][j];
 	}
 
 	Matrix& operator=(const Matrix& rhs){
@@ -67,29 +72,29 @@ public:
 			for (size_t i = 0; i < r; ++i)
 				mtr[i] = new T[c];
 			for (size_t i = 0; i < rhs.r; ++i)
-				for (size_t j = 0; j < rhs.c; ++j)
-					mtr[i][j] = rhs[i][j];
+			for (size_t j = 0; j < rhs.c; ++j)
+				mtr[i][j] = rhs[i][j];
 		}
 		return *this;
 	}
 
-	T* operator [](size_t i){return mtr[i];}
+	T* operator [](size_t i){ return mtr[i]; }
 	const T* const operator [](size_t i) const { return mtr[i]; }
 
 	Matrix operator +(const Matrix& rhs){
 		Matrix res = *this;
 		assert(r == rhs.r && c == rhs.c);
 		for (size_t i = 0; i < r; ++i)
-			for (size_t j = 0; j < c; ++j)
-				res[i][j] += rhs[i][j];
+		for (size_t j = 0; j < c; ++j)
+			res[i][j] += rhs[i][j];
 		return res;
 	}
 
 	Matrix& operator +=(const Matrix& rhs){
 		assert(r == rhs.r && c == rhs.c);
 		for (size_t i = 0; i < r; ++i)
-			for (size_t j = 0; j < c; ++j)
-				mtr[i][j] += rhs[i][j];
+		for (size_t j = 0; j < c; ++j)
+			mtr[i][j] += rhs[i][j];
 		return *this;
 	}
 
@@ -97,16 +102,16 @@ public:
 		Matrix res = *this;
 		assert(r == rhs.r && c == rhs.c);
 		for (size_t i = 0; i < r; ++i)
-			for (size_t j = 0; j < c; ++j)
-				res[i][j] -= rhs[i][j];
+		for (size_t j = 0; j < c; ++j)
+			res[i][j] -= rhs[i][j];
 		return res;
 	}
 
 	Matrix& operator -=(const Matrix& rhs){
 		assert(r == rhs.r && c == rhs.c);
 		for (size_t i = 0; i < r; ++i)
-			for (size_t j = 0; j < c; ++j)
-				mtr[i][j] -= rhs[i][j];
+		for (size_t j = 0; j < c; ++j)
+			mtr[i][j] -= rhs[i][j];
 		return *this;
 	}
 
@@ -114,9 +119,9 @@ public:
 		Matrix res(r, rhs.c, (T)0);
 		assert(c == rhs.r);
 		for (size_t i = 0; i < r; ++i)
-			for (size_t k = 0; k < rhs.c; ++k)
-				for (size_t j = 0; j < c; ++j)
-					res[i][k] += mtr[i][j] * rhs[j][k];
+		for (size_t k = 0; k < rhs.c; ++k)
+		for (size_t j = 0; j < c; ++j)
+			res[i][k] += mtr[i][j] * rhs[j][k];
 		return res;
 	}
 
@@ -127,11 +132,10 @@ public:
 	}
 
 	bool operator ==(const Matrix<T> &rhs){
-		const double epsilon = 1e-7;
 		if (c != rhs.c || r != rhs.r) return false;
 		for (size_t i = 0; i < r; ++i)
-			for (size_t j = 0; j < c; ++j)
-				if (abs((*this)[i][j] - rhs[i][j]) > epsilon) return false;
+		for (size_t j = 0; j < c; ++j)
+		if (mtr[i][j] != rhs[i][j]) return false;
 		return true;
 	}
 
@@ -155,8 +159,8 @@ public:
 		return *this;
 	}
 
-	size_t get_rows(){return r;}
-	size_t get_cols(){return c;}
+	size_t get_rows(){ return r; }
+	size_t get_cols(){ return c; }
 
 	~Matrix(){
 		for (size_t i = 0; i < r; ++i) delete[] mtr[i];
@@ -165,47 +169,59 @@ public:
 
 };
 
+
 template<class T>
-class JordanMatrix: public Matrix<T>{
-	vector <size_t> cells; //each element represents number of cells
-	
-	void calcCells(){
-		size_t sz = 1;
-		for (size_t i = 0; i < r; ++i)
-		if (i + 1 < r && mtr[i][i + 1] == (T)1)
-			++sz;
-		else{
-			cells.push_back(sz);
-			sz = 1;
-		}
+void reorderCells(Matrix<T>& a){
+	vector<size_t> cells;
+	size_t sz = 1;
+	size_t n = a.get_cols();
+	for (size_t i = 0; i < n; ++i)
+	if (i + 1 < n && a[i][i + 1] == (T)1)
+		++sz;
+	else{
+		cells.push_back(sz);
+		sz = 1;
 	}
-public:
-
-	JordanMatrix(size_t n, T **a){
-		r = n, c = n;
-		mtr = new T*[n];
-		for (size_t i = 0; i < n; ++i)
-			mtr[i] = new T[n];
-		for (size_t i = 0; i < n; ++i)
-			for (size_t j = 0; j < n; ++j)
-				mtr[i][j] = a[i][j];
-	}
-
-	void sortCells(){	
-		calcCells();
-		sort(cells.begin(), cells.end(), greater<size_t> ());
-		for (size_t i = 0; i < r - 1; ++i)
-			mtr[i][i + 1] = 0;
-		size_t i = 0;
-		for (auto csz : cells){
-			while (--csz){
-				mtr[i][i + 1] = 1;
-				++i;
-			}
+	sort(cells.begin(), cells.end(), greater<size_t>());
+	for (size_t i = 0; i < n - 1; ++i)
+		a[i][i + 1] = 0;
+	size_t i = 0;
+	for (auto csz : cells){
+		while (--csz){
+			a[i][i + 1] = 1;
 			++i;
 		}
+		++i;
 	}
-}; 
+}
+
+template<class T>
+void restoreUnits(Matrix<T>& A, Matrix<T>& J){
+	assert(A.get_rows() == J.get_rows());
+	assert(A.get_cols() == J.get_cols());
+	assert(A.get_cols() == A.get_rows());
+	size_t n = A.get_cols();
+	Matrix<T> I(n, n, (T)0);                                //identity matrix
+	for (size_t i = 0; i < n; ++i)
+		I[i][i] = (T)1;
+
+	for (size_t k = 0; k < n - 1; ++k){                     //iterate by units in J ( Jordan canonical form)
+		if (J[k][k + 1] != (T)1) continue;
+
+		Matrix<T> B = I;                                    //matrix B
+		B[k + 1][k + 1] = A[k][k + 1];
+		Matrix<T> RB = I;                                   //inverse matrix to B
+		RB[k + 1][k + 1] = (T)1 / A[k][k + 1];
+
+		A = B * A * RB;										//matrix transform
+		for (size_t i = 0; i < n; ++i){
+			if (i == k + 1) continue;
+			T ratio = A[k][i];
+			for (size_t j = 0; j < n; ++j)
+				A[j][i] -= A[j][k + 1] * ratio;
+		}
+	}
+}
 
 complex<double> randomComplex(double eps){
 	double r = eps * (double)rand() / (double)RAND_MAX;
@@ -218,82 +234,38 @@ complex<double> randomComplex(double eps){
 template<class T>
 void output(Matrix<T>* M){
 	for (size_t i = 0; i < M->get_rows(); ++i)
-		for (size_t j = 0; j < M->get_cols(); ++j)
-			cout << (*M)[i][j] << (j == M->get_cols() - 1 ? '\n' : ' ');
-}
-
-void test_sum(Matrix<complex<double> > M){
-	cout << "Sum test ...\n";
-	cout << "Before >> ..\n";
-	output(&M);
-	Matrix<complex<double> > tmp = M + M;
-	cout << "After M + M ..\n";
-	M += M;
-	output(&M);
-	assert(tmp == M);
-	cout << "End test+\n\n";
-}
-
-void test_multp(Matrix<complex<double> > M){
-	cout << "Multiplication test..\n";
-	cout << "Before..\n";
-	output(&M);
-	Matrix<complex<double> > tmp = M*M;
-	cout << "After M * M\n";
-	M *= M;
-	output(&M);
-	assert(tmp == M);
-	cout << "End test+\n\n";
-}
-
-void test_transp(Matrix<complex<double> > M){
-	cout << "Transpose test ...\n";
-	cout << "Before >> ..\n";
-	output(&M);
-	Matrix<complex<double> > tmp = M;
-	cout << "After first transpose ..\n";
-	M.transpose();
-	output(&M);
-	cout << "After second transpose ..\n";
-	M.transpose();
-	output(&M);
-	assert(tmp == M);
-	cout << "End test+\n\n";
-}
-
-void test_sort(JordanMatrix<complex<double> > J){
-	cout << "Sort cells test ...\n";
-	cout << "Before >> ..\n";
-	output(&J);
-	J.sortCells();
-	cout << "After >> ..\n";
-	output(&J);
-	cout << "End test+\n\n";
+	for (size_t j = 0; j < M->get_cols(); ++j){
+		T to = (*M)[i][j];
+		if (to == (T)0) to = (T)0;
+		if (to == (T)1) to = (T)1;
+		cout << setprecision(3) << to << (j == M->get_cols() - 1 ? '\n' : ' ');
+	}
 }
 
 int main(){
+	freopen("input.txt", "r", stdin);
 	srand(time(0));
 	int n;
 	double eps;
 	cin >> n >> eps;
 	complex<double> **a;
 	a = new complex <double>*[n];
-	for (int i = 0; i < n; ++i) a[i] = new complex<double>[n];
 	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j){
-			int num;
-			cin >> num;
-			a[i][j] = num;
-		}
-	JordanMatrix<complex<double> > J((size_t)n, a);
+		a[i] = new complex<double>[n];
+	for (int i = 0; i < n; ++i)
+	for (int j = 0; j < n; ++j){
+		int num;
+		cin >> num;
+		a[i][j] = num;
+	}
+	Matrix<complex<double> > J((size_t)n, (size_t)n, a);
 	Matrix <complex<double> > E(n, n, (complex<double>)0);
 	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j)
-			E[i][j] = randomComplex(eps);
+	for (int j = 0; j < n; ++j)
+		E[i][j] = randomComplex(eps);
 	Matrix <complex<double> > A = E + J;
-	test_sort(J);
-	test_transp(J);
-	test_sum(A);
-	test_multp(A);
+	reorderCells(J);
+	restoreUnits(A, J);
+	output(&A);
 	return 0;
 }
